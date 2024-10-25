3704087c4b1223b794020f230e752777c8c87dd6
wayphone <a0936239163@gmail.com>
2024-10-25 21:00:34 +0800

    level0 update

--- bandit_Level_0/bandit_level_0.py
+++ bandit_Level_0/bandit_level_0.py
@@ -1,2 +1,17 @@
 import paramiko
 
+#建立SSHClient
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+#連接伺服器
+ssh.connect(hostname='bandit.labs.overthewire.org', port=2220, username='bandit0', password='bandit0')
+
+#執行命令&輸出結果
+stdin, stdout, stderr = ssh.exec_command('ls')
+print(stdout.read().decode())
+stdin, stdout, stderr = ssh.exec_command('cat readme')
+print(stdout.read().decode())
+
+#關閉伺服器
+ssh.close()
